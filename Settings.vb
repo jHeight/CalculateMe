cfedef724633a9ec932da1698028d83d631dd0d1
SlipSerum <appsbygamer@yahoo.co.uk>
2017-05-11 21:28:52 -0700

    Update Settings.vb
    Added Save Method.

--- Settings.vb
+++ Settings.vb
@@ -1,9 +1,16 @@
+Imports System.IO
+Imports System.Text
+
 Public Class Form2
 
     Dim theme As String = "dark"
+    Dim path As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+    Dim file As System.IO.StreamWriter
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
+        file = My.Computer.FileSystem.OpenTextFileWriter(path & "\settings.xml", True)
+        file.WriteLine(theme)
+        file.Close()
     End Sub
 
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
