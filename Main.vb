5ae6db2324ff7aa058b68ee708c835ca45fb7ec1
SlipSerum <appsbygamer@yahoo.co.uk>
2017-05-11 21:39:22 -0700

    Update Main.vb
    Added Theme Reader and ThemeManager()

--- Main.vb
+++ Main.vb
@@ -1,13 +1,6 @@
-Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
-
-    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-        Application.Exit()
-    End Sub
-
-    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
-        Me.WindowState = FormWindowState.Minimized
-    End Sub
-End Class
+    Dim fileReader As System.IO.StreamReader
+    fileReader = My.Computer.FileSystem.OpenTextFileReader(path & "\settings.xml")
+    Dim stringReader As String
+    stringReader = fileReader.ReadLine()
+    
